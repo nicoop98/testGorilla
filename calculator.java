bee39628a150d2e75e28b7e38428ed2cffc024b2
lop427 <nicoop1998@gmail.com>
2022-03-17 19:06:06 +0100

    falta 1

--- calculator.java
+++ calculator.java
@@ -1,10 +1,26 @@
-public class main {
+package ejercicios;
 
-    String viaje1="Vienna-Prague 312, Prague-Zurich 676";
-    String viaje2="Vienna Prague 312, Prague Zurich 676";
+public class calculator {
 
-
-    public static String calculator(String[] distancesList) {
+	public static void main(String[] args) {
+		
+		String[] viaje1={"Vienna-Prague 312", "Prague-Zurich 676"};
+	    String[] viaje2={"Vienna Prague 312", "Prague Zurich 676"};
+	    String[] viaje3= {"trayecto1 10", " trayecto2 20"};
+	    
+	    String ap1=calculator(viaje1);
+	    String ap2=calculator(viaje2);
+	    String ap3=calculator(viaje3);
+	    
+	    System.out.println("\nActual: "+ap1.toString());
+	    System.out.println("\nExpected: Prague-Zurich 676, expenses: 160.55");
+	    System.out.println("\nActual: "+ap2.toString());
+	    System.out.println("\nExpected: Prague Zurich 676, expenses: 160.55");
+	    System.out.println("\nActual: "+ap3.toString());
+	    System.out.println("\nExpected: trayecto2 20, expenses: 4.88");
+	}
+	
+	public static String calculator(String[] distancesList) {
         final double perLitre = 1.30;
         final int consumption = 8;
         int totalDistance = 0;
@@ -12,7 +28,7 @@ public class main {
     
         for(int i=0; i<distancesList.length; i++){
             String item = distancesList[i];
-            String[] words = item.split(" ");
+            String[] words = item.split("[ -]");
             String distInKm = words[words.length-1];
             distances[i] = Integer.parseInt(distInKm);
         }
@@ -25,5 +41,9 @@ public class main {
                 max = i;
             }
         }
+        
+        
+		return null;
     }
+
 }
